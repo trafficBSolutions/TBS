e66bdfcef016153fad42e19887241fe97b1d2483
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-12-16 12:50:09 -0500

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,11 +6,9 @@ import BollardsWheels from './bollardswheels'
 import Signs from './signs'
 import Rentals from './rentals'
 import PPE from './ppe'
-import Email from './emailform'
 import Error from './error'
-import Web from './web-development'
 import TService from './traffic-control-services'
 import Product from './product-services'
 import Contact from './contact-us'
 import About from './about-us'
-export { About, Contact, Apply, Home, TrafficControl, TrafficPlan, BollardsWheels, Signs, Rentals, PPE, Email, Error, Web, TService, Product };
+export { About, Contact, Apply, Home, TrafficControl, TrafficPlan, BollardsWheels, Signs, Rentals, PPE, Error, TService, Product };
