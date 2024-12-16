6f670e507632478136489c52154495d608b3408e
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-12-16 11:42:42 -0500

    Update and rename g to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { Routes, Route, Link} from 'react-router-dom'
+import { About, Contact, Apply, Home, TrafficControl, TrafficPlan, Rentals, PPE, Signs, BollardsWheels, Email, Error, Web, TService, Product } from './pages';
+import axios from 'axios';
+import Navbar from './components/Navbar';
+import { Toaster } from 'react-hot-toast'
+
+
+ axios.defaults.baseURL = 'https://tbs-server.onrender.com'; 
+/* axios.defaults.baseURL = 'http://localhost:8000'; */
+axios.defaults.withCredentials = true
+
+
+function App() {
+  return (
+    <>
+<Navbar />
+  <Toaster position='bottom-right' toastOptions={{duration: 2000}} />
+  <Routes>
+    <Route path="/applynow" element={<Apply/>}/>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/trafficcontrol" element={<TrafficControl/>}/>
+    <Route path="/trafficplanning" element={<TrafficPlan/>}/>
+    <Route path="/rentals" element={<Rentals/>}/>
+    <Route path="/ppe" element={<PPE/>}/>
+    <Route path="/signs" element={<Signs/>}/>
+    <Route path="/bollardswheels" element={<BollardsWheels/>}/>
+    <Route path="/emailform" element={<Email/>}/>
+    <Route path="/web-development" element={<Web/>}/>
+    <Route path="*" element={<Error/>}/>
+    <Route path="/traffic-control-services" element={<TService/>}/>
+    <Route path="/product-services" element={<Product/>}/>
+    <Route path="/contact-us" element={<Contact/>}/>
+    <Route path="/about-us" element={<About/>}/>
+  </Routes>
+</>
+
+  );
+}
+
+export default App;
