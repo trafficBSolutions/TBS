1be77a0861fa78bfdc52837d7daffe4b8a7d1176
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2025-01-03 17:24:51 -0500

    Update applynow.jsx

--- src/pages/applynow.jsx
+++ src/pages/applynow.jsx
@@ -363,7 +363,7 @@ export default function Apply() {
   </div>
 </footer>
 <div className="footer-copyright">
-      <p className="footer-copy-p">&copy; 2024 Traffic & Barrier Solutions, LLC - 
+      <p className="footer-copy-p">&copy; 2025 Traffic & Barrier Solutions, LLC - 
         Website Created & Deployed by William Rowell - All Rights Reserved.</p>
     </div>
     </div>
