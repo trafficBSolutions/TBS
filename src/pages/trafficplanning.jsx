5fe15c0a9b164189871d3d92472bbc9b0595be2e
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2025-01-03 18:36:52 -0500

    Update trafficplanning.jsx

--- src/pages/trafficplanning.jsx
+++ src/pages/trafficplanning.jsx
@@ -506,7 +506,7 @@ Together, we can create safer roads, smoother traffic flow, and more resilient c
 </footer>
 <div className="footer-copyright">
       <p className="footer-copy-p">&copy; 2025 Traffic & Barrier Solutions, LLC - 
-        Website Created & Deployed by <a className="footer-face"href="https://www.facebook.com/will.rowell.779" target="_blank" rel="noopener noreferrer">William Rowell</a> - All Rights Reserved.</p>
+        Website MERN Stack Coded & Deployed by <a className="footer-face"href="https://www.facebook.com/will.rowell.779" target="_blank" rel="noopener noreferrer">William Rowell</a> - All Rights Reserved.</p>
     </div>
         </div>
     )
