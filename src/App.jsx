96006e3f37eccf96b2188a1e5b9c8944bba7840a
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2025-04-15 12:23:40 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Routes, Route, Link} from 'react-router-dom'
-import { CancelJob, About, Contact, Apply, Home, TrafficControl, TrafficPlan, Rentals, PPE, Signs, BollardsWheels, Error, TService, Product } from './pages';
+import { Routes, Route, Navigate} from 'react-router-dom'
+import { About, CancelJob, AdminLog, RegisterAdmin, AdminDashboard, Contact, TrafficSoftware, Apply, Home, TrafficControl, TrafficPlan, Rentals, PPE, Signs, BollardsWheels, Email, Error, Web, TService, Product } from './pages';
 import axios from 'axios';
 import Navbar from './components/Navbar';
 import { Toaster } from 'react-hot-toast'
-
+import { isAdminAuthenticated } from './utils/auth';
 
  axios.defaults.baseURL = 'https://tbs-server.onrender.com'; 
-/* axios.defaults.baseURL = 'http://localhost:8000'; */
+/* axios.defaults.baseURL = 'http://localhost:8000';*/
 axios.defaults.withCredentials = true
 
 
@@ -24,11 +24,19 @@ function App() {
     <Route path="/ppe" element={<PPE/>}/>
     <Route path="/signs" element={<Signs/>}/>
     <Route path="/bollardswheels" element={<BollardsWheels/>}/>
+    <Route path="/emailform" element={<Email/>}/>
+    <Route path="/web-development" element={<Web/>}/>
     <Route path="*" element={<Error/>}/>
     <Route path="/traffic-control-services" element={<TService/>}/>
     <Route path="/product-services" element={<Product/>}/>
     <Route path="/contact-us" element={<Contact/>}/>
     <Route path="/about-us" element={<About/>}/>
+    <Route path="/traffic-software" element={<TrafficSoftware/>}/>
+    <Route path="/admin-login" element={<AdminLog />} />
+    <Route path="/admin-dashboard"
+        element={isAdminAuthenticated() ? <AdminDashboard /> : <Navigate to="/admin-login" />}
+/>
+    <Route path="/register-admin" element={<RegisterAdmin />} />
     <Route path="/cancel-job/:id" element={<CancelJob />} />
   </Routes>
 </>
