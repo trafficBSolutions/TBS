bdf0550adf0a5d7b4756d9e2c7055017cd03a6ee
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2025-04-21 17:21:38 -0400

    Update constantapi.js

--- src/constants/constantapi.js
+++ src/constants/constantapi.js
@@ -1,1 +1,1 @@
-export const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY || "AIzaSyD5ryBoXHk4ioi6INEKO9_1sZiAxTTSCLo";
+export const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
