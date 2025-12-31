e7e876b3f3f4da3274ecd98916d5f55b5ee9409e
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-12-31 16:09:42 -0500

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -245,7 +245,7 @@ export default function Home() {
   </div>
 </footer>
 <div className="footer-copyright">
-      <p className="footer-copy-p">&copy; 2025 Traffic & Barrier Solutions, LLC - 
+      <p className="footer-copy-p">&copy; 2026 Traffic & Barrier Solutions, LLC - 
         Website Created & Deployed by <a className="footer-face"href="https://www.facebook.com/will.rowell.779" target="_blank" rel="noopener noreferrer">William Rowell</a> - All Rights Reserved.</p>
     </div>
             </div>
