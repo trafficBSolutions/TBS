bfeb70b73d60e3d120546eb1aa3504a44dcdb579
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-02-02 14:55:29 -0500

    Expand options for Arrow Board selection

--- src/pages/work-order.jsx
+++ src/pages/work-order.jsx
@@ -944,7 +944,7 @@ const isSubmitReady = useMemo(() => {
         {selectPair('hardHats','Hard Hats',[2,3,4,5,6])}
                   {selectPair('vests','Vests',[2,3,4,5,6])}
                   {selectPair('walkies','Walkie Talkies',[2,3,4,5,6])}
-                  {selectPair('arrowBoards','Arrow Board',[0,1,2])}
+                  {selectPair('arrowBoards','Arrow Board',[0,1,2,3,4])}
 
                   {numberInput('cones','Cones')}
                   {numberInput('barrels','Barrels')}
