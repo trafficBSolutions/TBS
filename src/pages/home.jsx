d17e6f32ca0e1b09a5fe29bfe4eea0332d5b9830
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-02-12 10:36:19 -0500

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -49,20 +49,20 @@ export default function Home() {
   </p>
 
   <div className="will-contact-link">
-    <h2 className="will-contact">Talk to William Rowell</h2>
+    <h2 className="will-contact">Contact Us for a Quote</h2>
     <p className="contact-web-info">
       <a className="will-phone" href="tel:+17068790106">
-        📞 Call/Text: (706) 879-0106
+        📞 Call/Text: (706) 263-0175
       </a>
       <br />
-      <a className="will-email" href="mailto:tbsolutions9@gmail.com">
-        📧 Email: tbsolutions9@gmail.com
+      <a className="will-email" href="mailto:tbsolutions1999@gmail.com">
+        📧 Email: tbsolutions1999@gmail.com
       </a>
     </p>
 
-    <a className="home-web-button" href="https://www.material-worx.com/new-website">Start Your Website</a>
+    <a className="home-web-button" href="https://www.material-worx.com/new-website">Get a Website Quote</a>
     <a className="home-web-button" href="https://william-rowell.netlify.app/" target="_blank" rel="noopener noreferrer">
-      View My Work
+      View Some Work
     </a>
   </div>
 </div>
