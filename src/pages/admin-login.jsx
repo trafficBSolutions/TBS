ec62bba56ffd553fb2b5ffdd8813b3fee5a3862e
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-04-14 13:07:02 -0400

    Update admin-login.jsx

--- src/pages/admin-login.jsx
+++ src/pages/admin-login.jsx
@@ -144,7 +144,7 @@ const AdminLog = () => {
 </footer>
 <div className="footer-copyright">
       <p className="footer-copy-p">&copy; 2026 Traffic & Barrier Solutions, LLC - 
-        Website Created & Deployed by <a className="footer-face"href="https://www.facebook.com/will.rowell.779" target="_blank" rel="noopener noreferrer">William Rowell</a> - All Rights Reserved.</p>
+        Website Created by <a className="footer-face"href="https://www.material-worx.com/portfolio" target="_blank" rel="noopener noreferrer">MX Systems</a> - All Rights Reserved.</p>
     </div>
       </div>
     );
