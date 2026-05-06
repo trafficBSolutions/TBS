53b63586cfbc7f11dae1f4983d2467388be48dd1
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-05-06 14:56:36 -0400

    Add route for ShopWorkOrderStatus component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate} from 'react-router-dom'
 import { About, EmployeeHandbook, Quote, ConfirmAdditionalFlagger, EmployeeDiscipline, Invoice, Work, ManageJobTest, CancelJobTest, ManageJob, CancelJob, AdminLog, AdminDashboard, Contact, Apply, Home, TrafficControl, TrafficPlan, Rentals, PPE, Signs, BollardsWheels, Error, TService, Product, TrafficControlTest, EmployeeDashboard, EmployeeComplaintForm } from './pages';
 import TCPDesigner from './pages/tcp-designer';
 import ShopWorkOrder from './pages/shop-work-order';
+import ShopWorkOrderStatus from './pages/shop-work-order-status';
 import axios from 'axios';
 import RescheduleJob from './pages/RescheduleJob';
 import Navbar from './components/Navbar';
@@ -128,6 +129,7 @@ function App() {
     <Route path="/employee-dashboard/work-order" element={<RequireStaff><Work /></RequireStaff>} />
     <Route path="/employee-dashboard/shop-work-order" element={<RequireStaff><ShopWorkOrder /></RequireStaff>} />
     <Route path="/admin-dashboard/shop-work-order" element={<RequireStaff><ShopWorkOrder /></RequireStaff>} />
+    <Route path="/shop-work-order/:status/:id" element={<ShopWorkOrderStatus />} />
     <Route path="/employee-dashboard/employee-complaint-form" element={<RequireStaff><EmployeeComplaintForm /></RequireStaff>} />
     <Route path="/employee-dashboard/employee-handbook" element={<RequireStaff><EmployeeHandbook /></RequireStaff>} />
     <Route path="/cancel-job/:id" element={<CancelJob />} />
