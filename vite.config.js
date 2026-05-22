e3ec5922bf4560d7b24a4c0f47d24cdf937e945b
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-05-22 09:51:18 -0400

    Increase maximum cache file size to 15MB

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 15 * 1024 * 1024,
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         runtimeCaching: [
           {
