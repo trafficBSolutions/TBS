92697f7f16ac6ce3293fccd884ae8b31ae929750
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-06-29 13:09:43 -0400

    Add index.js to export multiple hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,6 @@
+export { default as useAdminAuth } from './useAdminAuth';
+export { default as useTasks } from './useTasks';
+export { default as useTrafficJobs } from './useTrafficJobs';
+export { default as useWorkOrders } from './useWorkOrders';
+export { default as useSignShop } from './useSignShop';
+export { default as useShopWorkOrders } from './useShopWorkOrders';
