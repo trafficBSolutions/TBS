2433edd02f9c525887b1177196ff2ebfce5bc72a
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-06-29 13:49:13 -0400

    Set default value for signShopList prop

--- src/components/admin/SignShopView.jsx
+++ src/components/admin/SignShopView.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SignShopView = ({
-  signShopDate, signShopList,
+  signShopDate, signShopList = [],
   signShopTitle, setSignShopTitle, signShopCustomer, setSignShopCustomer,
   signShopDesc, setSignShopDesc, signShopPhotos, setSignShopPhotos,
   editingSignShopId, editSignShop, setEditSignShop, editSignShopPhotos, setEditSignShopPhotos,
