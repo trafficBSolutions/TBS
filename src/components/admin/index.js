2b651322cf971541e74e443746eb169d600e61b8
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-06-29 13:44:05 -0400

    Add QuotesView and layout components to admin index
    Added new QuotesView and layout components for Admin.

--- src/components/admin/index.js
+++ src/components/admin/index.js
@@ -7,7 +7,7 @@ export { default as AdminFooter } from './AdminFooter';
 export { default as LeaveRequestsSection } from './LeaveRequestsSection';
 export { default as PendingShopWoSection } from './PendingShopWoSection';
 
-// New view components
+// View components
 export { default as TasksView } from './TasksView';
 export { default as TrafficJobsView } from './TrafficJobsView';
 export { default as WorkOrdersView } from './WorkOrdersView';
@@ -15,3 +15,9 @@ export { default as BollardsView } from './BollardsView';
 export { default as ComplaintsView } from './ComplaintsView';
 export { default as SignShopView } from './SignShopView';
 export { default as ShopWorkOrdersView } from './ShopWorkOrdersView';
+export { default as QuotesView } from './QuotesView';
+
+// Layout components
+export { default as AdminTopBar } from './AdminTopBar';
+export { default as AdminCalendar } from './AdminCalendar';
+export { default as AdminViewRenderer } from './AdminViewRenderer';
