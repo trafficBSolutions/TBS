62b4e3ea5cfefee0e63af03ffd746ab23773d1a5
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-07-13 12:09:51 -0400

    Add support for MOV file extension in image imports

--- src/utils/tbsImages.js
+++ src/utils/tbsImages.js
@@ -1,4 +1,4 @@
-const images = import.meta.glob('../assets/**/*.{png,jpg,jpeg,svg,mp4,pdf}', { eager: true });
+const images = import.meta.glob('../assets/**/*.{png,jpg,jpeg,svg,mp4,mov,MOV,pdf}', { eager: true });
 
 console.log(images); // Log the keys to see the paths Vite generates
 
