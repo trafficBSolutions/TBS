718a1fd7a1752c85957ed6c8de48946264f46211
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-08-03 11:49:49 -0400

    Add DirectDeposit to exports in index.jsx
    Added DirectDeposit import and export to index.jsx.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -35,4 +35,5 @@ import ShopWorkOrderStatus from './shop-work-order-status'
 import TimeClockKiosk from './time-clock-kiosk'
 import ShopWorkOrderLog from './shop-work-order-log'
 import HydrovacServices from './hydrovac-services'
-export { HydrovacServices, ShopWorkOrderLog, TimeClockKiosk, LeaveRequest, AdminDashboard, ShopWorkOrderStatus, ShopWorkOrder, TCPDesigner, Quote, EmployeeHandbook, RescheduleJob, EmployeeDiscipline, ConfirmAdditionalFlagger, EmployeeLogin, EmployeeDashboard, EmployeeComplaintForm, Work, Invoice, CancelJob, About, Contact, Apply, Home, AdminLog, TrafficControl, TrafficPlan, BollardsWheels, Signs, Rentals, PPE, Error, TService, Product, ManageJob, TrafficControlTest, ManageJobTest, CancelJobTest };
+import DirectDeposit from './direct-deposit'
+export { DirectDeposit, HydrovacServices, ShopWorkOrderLog, TimeClockKiosk, LeaveRequest, AdminDashboard, ShopWorkOrderStatus, ShopWorkOrder, TCPDesigner, Quote, EmployeeHandbook, RescheduleJob, EmployeeDiscipline, ConfirmAdditionalFlagger, EmployeeLogin, EmployeeDashboard, EmployeeComplaintForm, Work, Invoice, CancelJob, About, Contact, Apply, Home, AdminLog, TrafficControl, TrafficPlan, BollardsWheels, Signs, Rentals, PPE, Error, TService, Product, ManageJob, TrafficControlTest, ManageJobTest, CancelJobTest };
